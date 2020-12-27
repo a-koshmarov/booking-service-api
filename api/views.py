2dc9f6ff9b7a4e941e03f5edeea4b1c52b38bb8e
Alexander <35034325+a-koshmarov@users.noreply.github.com>
2020-12-27 08:44:17 +0300

    added swagger annotations

--- api/views.py
+++ api/views.py
@@ -5,11 +5,18 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+
 from .models import Booking
 from .serializers import BookingSerializer, UserSerializer
 
 class BookingView(APIView):
 
+    display_all_param = openapi.Parameter('all', openapi.IN_QUERY, description="Display all bookings", type=openapi.TYPE_BOOLEAN)
+    datetime_from_param = openapi.Parameter('datetime_from', openapi.IN_QUERY, description="Display bookings starting from datetime (accepts iso datetime)", type=openapi.TYPE_STRING)
+    datetime_to_param = openapi.Parameter('datetime_to', openapi.IN_QUERY, description="Display bookings ending by datetime (accepts iso datetime)", type=openapi.TYPE_STRING)
+    @swagger_auto_schema(manual_parameters=[display_all_param, datetime_from_param, datetime_to_param])
     def get(self, request):
         display_all = request.GET.get('all', '')
         datetime_from = request.GET.get('datetime_from', '')
@@ -40,6 +47,8 @@ class BookingView(APIView):
 
 class RoomView(APIView):
     
+    display_all_param = openapi.Parameter('all', openapi.IN_QUERY, description="Display all bookings", type=openapi.TYPE_BOOLEAN)
+    @swagger_auto_schema(manual_parameters=[display_all_param])
     def get(self, request, room):
         display_all = request.GET.get('all', '')
 
