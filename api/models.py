e345fa1d81aee4d1233cc5ae25953c628437f868
Alexander <35034325+a-koshmarov@users.noreply.github.com>
2020-12-27 16:53:17 +0300

    Update models.py

--- api/models.py
+++ api/models.py
@@ -9,14 +9,14 @@ from rest_framework.authtoken.models import Token
 class User(AbstractUser):
     pass
 
-@receiver(pre_save, sender=settings.AUTH_USER_MODEL)
-def my_callback(sender, instance, *args, **kwargs):
-    instance.is_active = True
+# @receiver(pre_save, sender=settings.AUTH_USER_MODEL)
+# def my_callback(sender, instance, *args, **kwargs):
+#     instance.is_active = True
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
+# @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+# def create_auth_token(sender, instance=None, created=False, **kwargs):
+#     if created:
+#         Token.objects.create(user=instance)
 
 class Room(models.Model):
     name = models.CharField(max_length=50)
