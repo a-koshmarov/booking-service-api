bb6fc2700e2ce45c6902b1fa159e0eb92ccec6b7
Alexander <35034325+a-koshmarov@users.noreply.github.com>
2020-12-27 18:36:36 +0300

    updated urls

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BookingView, RoomView, MakeBookingView, LoginView, RegisterView, CreateUserView
+from .views import BookingView, RoomView, MakeBookingView, LoginView, CreateUserView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
